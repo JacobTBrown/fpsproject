5109f981af9a8645ff2f409f5918c645f547e51d
zmerson <zemerson@uco.edu>
2022-09-19 13:39:52 -0500

    Added Photon & Text-mesh-pro

---
+++ FPSProject/Assets/Scripts/Zach/Launcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Attemting to connect");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
