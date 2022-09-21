81c68a26f1ed04c82aca7186e21541bcadd1b6d1
JacobTBrown <jacobtylerbrown@hotmail.com>
2022-09-21 16:07:28 -0500

    PlayerMovement Rewrite + Added Camera Control
    Rewrote PlayerMovement using Rigidbodies. Added camera control for the player.

---
+++ FPSProject/Assets/Scripts/Jacob/PlayerCameraMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Author: Jacob Brown
+    Creation: 9/20/22
+    Last Edit: 9/21/22
+*/
+public class PlayerCameraMovement : MonoBehaviour
+{
+    [Header("Unity Classes")]
+    public Transform cameraTransform;
+    public Transform playerTransform;
+    [Header("User Mouse Settings")]
+    // Horizontal mouse sensitivity
+    public float mouseXSensitivity = 250f;
+    // Vertical mouse sensitivity
+    public float mouseYSensitivity = 250f;
+    // For the user to invert the Y mouse look
+    public bool invertMouse = false;
+    private float rotateYAxis, rotateXAxis;
+    private Vector3 mouseMovement = new Vector3(0, 0, 0);
+    private PlayerMovement playerMove;
+    
+    void Start()
+    {
+        playerMove = transform.parent.gameObject.GetComponent<PlayerMovement>();
+        // Lock the cursor to the center of the screen 
+        Cursor.lockState = CursorLockMode.Locked;
+        // Make the cursor invisible
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        GetInputs();
+        InvertMouse(); 
+        PerformRotation();
+    }
+
+    private void GetInputs() {
+        // Get the horizontal mouse movement * sensitivity
+        mouseMovement.x = Input.GetAxis("Mouse X") * mouseXSensitivity;
+        // Get the vertical mouse movement * sensitivity
+        mouseMovement.y = Input.GetAxis("Mouse Y") * mouseYSensitivity;
+        mouseMovement *= Time.deltaTime;
+    }
+
+    private void InvertMouse() {
+        // Setting for the user to invert the mouse when looking up and down
+        if (invertMouse)
+            rotateXAxis += mouseMovement.y;
+        else
+            rotateXAxis -= mouseMovement.y;
+    }
+
+    private void PerformRotation() {
+        rotateYAxis += mouseMovement.x;
+        // Clamp the mouse rotation X so that we can't look over and backwards.
+        // By default the camera is looking forward at an x angle of 0, hence we use
+        // -90f and 90f to prevent going too far down or too far up.
+        rotateXAxis = Mathf.Clamp(rotateXAxis, -90f, 90f);
+        cameraTransform.rotation = Quaternion.Euler(rotateXAxis, rotateYAxis, 0f);
+        // If the player is on the ground. Then rotate the player on the y axis so that 
+        // upon moving, we move relative to the player's orientation. This way
+        // if we are in the air turning the camera doesn't alter the trajectory.
+        if (playerMove.isOnGround)
+            playerTransform.rotation = Quaternion.Euler(0f, rotateYAxis, 0f);
+    }
+}
--- FPSProject/Assets/Scripts/Jacob/PlayerMovement.cs
+++ FPSProject/Assets/Scripts/Jacob/PlayerMovement.cs
@@ -5,54 +5,121 @@ using UnityEngine;
 /*
     Author: Jacob Brown
     Creation: 9/19/22
-    Last Edit: 9/20/22
+    Last Edit: 9/21/22
 */
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    [Header("Unity Classes")]
+    public Rigidbody playerRigidbody;
     public Transform playerTransform;
-    public Vector3 velocity = new Vector3(0, 0, 0);
-    public Vector3 gravity = new Vector3(0, -9.81f, 0);
-    private Vector3 movement;
+    public LayerMask groundLayer;
+    
+    [Header("Movement Variables")]
+    // All values are set in the inspector
+    public float groundDrag;
+    public float groundSpeed;
+    public float airDrag;
+    public float airSpeed;
+    public float jumpForce;
+    public float jumpTimer;
+    public bool isOnGround;
+    public bool canJump;
+    
+    private float horizontalXInput;
+    private float horizontalZInput;
+    private Vector3 movement, velocity, limitedVelocity;
 
-    // Movement speed along x-z axis
-    public float movementSpeed = 4.0f;
-    public float jumpHeight = 5.0f;
-    // Update is called once per frame
+    void Start() {
+        // If we don't do this the player will fall over because it is a capsule
+        playerRigidbody.freezeRotation = true;
+    }
+    
     void Update()
     {
-        // Using GetAxis insures that if we want to use a controller no extra work will 
-        // be necessary, this function ranges from 1 to -1.
-        velocity.x = Input.GetAxis("Horizontal");
-        velocity.z = Input.GetAxis("Vertical");
-
-        Jump();
-
-        // Set up our movement vector
-        movement = playerTransform.right * velocity.x + playerTransform.forward * velocity.z;
-        // Multiply by our move speed and Time.deltaTime for smooth movement
-        movement *= movementSpeed * Time.deltaTime;
-        // Add the vertical axis as well for jumping
-        movement += playerTransform.up * velocity.y * Time.deltaTime;
-        // Moves our player based on the x-y-z of the movement vector
-        controller.Move(movement);
+        CheckForGround();
+        GetInputs();
+        ApplyDrag();
     }
 
+    void FixedUpdate() {
+        Move();
+    }
+
+    private void CheckForGround() {
+        /* This raycast simply points downwards from the player's position. It extends to half
+        // the player's height + 0.1f. */
+        isOnGround = Physics.Raycast(playerTransform.position, Vector3.down, 1.1f, groundLayer);
+    }
+
+    private void GetInputs() {
+        /* This function ranges from 1 to -1. */
+        horizontalXInput = Input.GetAxis("Horizontal");
+        horizontalZInput = Input.GetAxis("Vertical");
+        
+        /* If the player presses the jump button, the player is grounded, and can jump are all true.*/
+        if (Input.GetButton("Jump") && canJump && isOnGround) {
+            Jump();
+        }
+    }
+    
     private void Jump() {
-        // If the player is mid-air
-        if (!controller.isGrounded) {
-            // Add gravity.y * Time.deltaTime simulating gravity
-            velocity.y += gravity.y * Time.deltaTime;
+        // Then set canJump = false, perform the jump, and use Invoke to call SetCanJumpTrue
+        // after a set amount of time i.e. jumpTimer. */
+        canJump = false;
+        
+        // Reset the rigidbody y velocity to start all jumps at the same baseline
+        playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+        // Add an upwards impulse to the player rigidbody multiplied by the jumpForce
+        playerRigidbody.AddForce(playerTransform.up * jumpForce, ForceMode.Impulse);
+
+        /* Without resetting on a timer, the jumps can be inconsistent. Most times
+        // a jump is executed as expected, but sometimes you will get an extra large jump. */
+        Invoke(nameof(SetCanJumpTrue), jumpTimer);
+    }
+
+    private void SetCanJumpTrue() {
+        canJump = true;
+    }
+
+    private void Move() {
+        // Set up our movement vector
+        movement = playerTransform.right * horizontalXInput + playerTransform.forward * horizontalZInput;
+        
+        // Moves our player based on the x-y-z of the movement vector
+        if (isOnGround) {
+            // If we are on the ground we want to use our groundSpeed
+            playerRigidbody.AddForce(movement.normalized * groundSpeed, ForceMode.Force);  
+        } else {
+            /* If we are in the air we want to be able to move just a little bit but not 
+            // like we are still grounded. So, airSpeed should typically be less than half
+            // of groundSpeed. */
+            playerRigidbody.AddForce(movement.normalized * airSpeed, ForceMode.Force);
+        }   
+
+        LimitMovementSpeed();
+    }
+
+    private void LimitMovementSpeed() {
+        /* A temporary velocity variable, we are only concerned with x-z movement. */
+        velocity = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+
+        /* This will ensure our player cannot accelerate infinitely and is capped to our
+        // groundSpeed variable. */
+        if (velocity.magnitude > groundSpeed) {
+            limitedVelocity = velocity.normalized * groundSpeed;
+            playerRigidbody.velocity = new Vector3(limitedVelocity.x, playerRigidbody.velocity.y, limitedVelocity.z);
         }
-        // If the player presses the jump button and the player is on the ground
-        if (Input.GetButton("Jump") && controller.isGrounded) {
-            // Set the vertical velocity to jumpHeight * -gravity.t clamped by the jumpHeight
-            velocity.y = Mathf.Clamp(jumpHeight * -gravity.y, -1, jumpHeight);
-        }
-        // If the player is on the ground and the vertical velocity is negative (gravity being applied)
-        if (controller.isGrounded && velocity.y < 0) {
-            // Reset the velocity y to a nice number
-            velocity.y = -1f;
-        }
+    }
+
+    private void ApplyDrag() {
+        /* If the player is on the ground, then we want to prevent the player from moving forever
+        // without any input.
+        // So, we set drag to our groundDrag (Value set in inspector).
+
+        // If the player is NOT on the ground, we set drag to airDrag. */
+        if (isOnGround)
+            playerRigidbody.drag = groundDrag;
+        else
+            playerRigidbody.drag = airDrag;
     }
 }
