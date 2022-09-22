de00bb9cccba6c7d4825678da57c1b0a703d6e19
cfisher5693 <cfisher5693@gmail.com>
2022-09-22 17:13:50 -0500

    TWO WORLD WARS
    Added pistol, weapons system, gun models, sound effects, particle effects, pistol animations.

---
+++ FPSProject/Assets/Scripts/Coleman/Gun.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GunData gunData;
+    [SerializeField] private Transform muzzle;
+    [SerializeField] private ParticleSystem flash;
+    Animator animator;
+    AudioSource[] sounds;
+    AudioSource gunshot;
+    AudioSource reload;
+
+    float timeSinceLastShot;
+    private void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+        PlayerShoot.reloadInput += ReloadInit;
+        animator = GetComponent<Animator>();
+        sounds = GetComponents<AudioSource>();
+        gunshot = sounds[0];
+        reload = sounds[1];
+    }
+
+    private IEnumerator Reload()
+    {
+        gunData.isReloading = true;
+        reload.Play();
+        yield return new WaitForSeconds(gunData.reloadTime);
+        gunData.currentAmmo = gunData.magSize;
+        gunData.isReloading = false;
+    }
+
+    public void ReloadInit()
+    {
+        if(!gunData.isReloading)
+        {
+            StartCoroutine(Reload());
+            animator.SetTrigger("Reload");
+        }
+    }
+
+    private bool canShoot()
+    {
+        if (!gunData.isReloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Shoot()
+    {
+        if (gunData.currentAmmo > 0)
+        {
+            if(canShoot())
+            {
+                if (Physics.Raycast(muzzle.position, transform.forward, out RaycastHit hitInfo, gunData.maxDistance))
+                {
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
+                    damageable?.Damage(gunData.damage);
+                }
+                gunData.currentAmmo--;
+                timeSinceLastShot = 0;
+                OnGunShot();
+            }    
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+        Debug.DrawRay(muzzle.position, muzzle.forward);
+    }
+
+    private void OnGunShot()
+    {
+        flash.Play();
+        gunshot.Play();
+        animator.SetTrigger("Shoot");
+    }
+}
---
+++ FPSProject/Assets/Scripts/Coleman/GunData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Gun", menuName="Weapon/Gun")]
+
+public class GunData : ScriptableObject
+{
+    [Header("Info")]
+    public new string name;
+    [Header("Shooting")]
+    public float damage;
+    public float maxDistance;
+    [Header("Reloading")]
+    public int currentAmmo;
+    public int magSize;
+    public float fireRate;
+    public float reloadTime;
+    public bool fullAuto;
+    [HideInInspector]
+    public bool isReloading;
+}
