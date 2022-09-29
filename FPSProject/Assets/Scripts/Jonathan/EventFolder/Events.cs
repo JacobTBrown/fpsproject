afb73011eff12828fe0dd58649d9107aeb9a28bf
jalexander26 <49921087+jalexander26@users.noreply.github.com>
2022-09-29 17:56:00 -0500

    Respawn Commit

---
+++ FPSProject/Assets/Scripts/Jonathan/EventFolder/Events.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Scripts.Jonathan{
+ /*
+        For all Game Events Please add any New Events to this Script
+
+    
+        - Edited By Jonathan Alexander on 9/27/2022 11:33pm
+ */
+    public static class Events
+    {
+         public static PlayerDeathEvent PlayerDeathEvent = new PlayerDeathEvent();
+    }
+
+    public class PlayerDeathEvent : GameEvent { 
+        public GameObject player;
+    }
+
+    
+}
