4fdf0573a88e37315a62943c21da55aa8c40c5f5
Blake-Brooks <brooks.blake.a@gmail.com>
2022-10-21 02:19:29 -0500

    changed branch

--- FPSProject/Assets/ScoreboardScript.cs
+++ FPSProject/Assets/ScoreboardScript.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Photon.Realtime;
+using Unity.Scripts.Jonathan;
 
 public class ScoreboardScript : MonoBehaviour
 {
     public TMP_Text usernameText;
     public TMP_Text killsText;
     public TMP_Text deathsText;
+    public int deaths;
     private Player player;
 
     public void Initialize(Player _player)
@@ -20,5 +22,22 @@ public class ScoreboardScript : MonoBehaviour
 
     private void Update() {
         usernameText.text = player.NickName;
+    }
+
+    private void Awake()
+    {
+        EventManager.AddListener<PlayerDeathEvent>(OnPlayerDeath);
+    }
+
+    void OnPlayerDeath(PlayerDeathEvent evt)
+    {
+        //HandlePlayerSpawn(evt.player);
+        Debug.Log("This player is: " + player.NickName + "Dead player is: " + evt.player.GetComponent<PlayerSettings>().nickname);
+        if (evt.player.GetComponent<PlayerSettings>().nickname.Equals(player.NickName))
+        {
+            deaths += 1;
+            deathsText.text = deaths.ToString();
+            Debug.Log("Number of deaths are: " + deaths);
+        }
     }
 }
