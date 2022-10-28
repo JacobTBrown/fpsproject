8f07233fb899ee7661ca463184a70153bcf1a7e9
zmerson <zemerson@uco.edu>
2022-10-28 18:24:45 -0500

    Removed merge conflict tags in PlayeShoot.cs

--- FPSProject/Assets/Scripts/Coleman/PlayerShoot.cs
+++ FPSProject/Assets/Scripts/Coleman/PlayerShoot.cs
@@ -21,33 +21,19 @@ public class PlayerShoot : MonoBehaviour
     private void Update()
     {
         Gun gun = GetComponentInChildren<Gun>();
-        if (PV.IsMine) {
+        if (PV.IsMine)
+        {
             if (!DataManager.Instance.IsCanShoot) return;
-<<<<<<< HEAD
-
-            Gun gun = GetComponentInChildren<Gun>();
-            if (!gun.gunData.fullAuto && gun.gameObject.activeSelf) 
+            if (!gun.gunData.fullAuto && gun.gameObject.activeSelf)
             {
-            //{
                 if (Input.GetMouseButtonDown(0)) shootInput?.Invoke();
-            //} else
-            //{
-                if (Input.GetMouseButton(0)) shootInput?.Invoke();
-            //}
-                if (Input.GetKeyDown(reloadKey)) reloadInput?.Invoke();
-                if (Input.GetKeyDown(KeyCode.E)) pickupInput?.Invoke();
             }
-=======
-        if (!gun.gunData.fullAuto && gun.gameObject.activeSelf)
-        {
-            if (Input.GetMouseButtonDown(0)) shootInput?.Invoke();
-        } else
-        {
-            if (Input.GetMouseButton(0)) shootInput?.Invoke();
-        }
+            else
+            {
+                if (Input.GetMouseButton(0)) shootInput?.Invoke();
+            }
             if (Input.GetKeyDown(reloadKey)) reloadInput?.Invoke();
             if (Input.GetKeyDown(KeyCode.E)) pickupInput?.Invoke();
->>>>>>> dcb61ce94349c7b24a84bc23b9501d9c56864f51
         }
     }
 }
