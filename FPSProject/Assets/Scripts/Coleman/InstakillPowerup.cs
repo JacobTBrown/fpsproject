868c630d0c8acf013fcca966143c6938e2227f18
JacobTBrown <jacobtylerbrown@hotmail.com>
2022-11-07 12:42:34 -0600

    Added Instakill powerup and progrids for probuilder

---
+++ FPSProject/Assets/Scripts/Coleman/InstakillPowerup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstakillPowerup : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem HealthPickupEffect;
+    public AudioSource powerupSound;
+    public GameObject parent;
+    public float duration = 5f;
+
+    public void Start()
+    {
+        powerupSound = GetComponent<AudioSource>();
+        parent = transform.parent.gameObject;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(KillPowerup(other));
+        }
+    }
+
+    IEnumerator KillPowerup(Collider player)
+    {
+        Gun[] gun = player.transform.parent.GetComponentsInChildren<Gun>();
+        powerupSound.Play();
+        foreach (Transform child in parent.gameObject.transform)
+        {
+            child.GetComponent<MeshRenderer>().enabled = false;
+        }
+        transform.parent.position = new Vector3(0, -100, 0);
+        for (int i = 0; i < gun.Length; i++) {
+            gun[i].isInstakill = true;
+        }
+
+        float time = 0;
+        while (time <= duration) {
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        for (int i = 0; i < gun.Length; i++) {
+            gun[i].isInstakill = false;
+        }
+
+        yield return new WaitForSeconds(1);
+        Destroy(transform.parent.gameObject);
+        
+        yield return null;
+    }
+}
